a9ff409669b3e717730a164d5650f24ab4ea94d7
eng.aladinhamod@gmail.com <eng.aladinhamod@gmail.com>
2019-08-10 22:17:43 +0100

    DataAnotation

--- Students.Data/Student.cs
+++ Students.Data/Student.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Students.Data
@@ -8,7 +7,7 @@ namespace Students.Data
     
     public class Student
     {
-        [Key]
+        
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
